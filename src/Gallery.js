5bf8ae0e976a33f41ef6746fe24c878fb42e94e2
Daniel Kapper <dkapper01@gmail.com>
2020-04-29 09:08:03 -0400

    added seconds const

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,25 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import PICTURES from './data/pictures';
 
+const SECONDS = 1000;
+
 function Gallery() {
   const [index, setIndex] = useState(0);
+  const [delay, setDelay] = useState(3 * SECONDS);
+  const [increment, setIncrement] = useState(1);
 
   useEffect(() => {
+    console.log('delay', delay);
     const interval = setInterval(() => {
       setIndex((storedIndex) => {
         return (storedIndex + 1) % PICTURES.length;
       });
-    }, 3000);
+    }, 3 * SECONDS);
     return () => {
-      clearInterval(interval)
-    }
-  }, []);
+      clearInterval(interval);
+    };
+  }, [delay]);
 
-  // console.log('index', index);
+  const updateDelay = (event) => {
+    setDelay(Number(event.target.value) * SECONDS);
+  };
 
   return (
-    <div className='gallery'>
+    <div className='Gallery'>
       <img src={PICTURES[index].image} />
+      <div className='multiform'>
+        <div>
+          Gallery transition delay (seconds)
+          <input type='number' onChange={updateDelay} />
+        </div>
+      </div>
     </div>
   );
 }
